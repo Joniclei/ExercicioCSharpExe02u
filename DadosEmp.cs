23eb501f7556ec0a5da8f88a7ec922a96c608cf4
Joniclei <jonicleibernardojunior@gmail.com>
2023-05-25 17:11:46 -0300

    Exercicio 02

---
+++ DadosEmp.cs
@@ -0,0 +1,27 @@
+namespace ExercicioB
+{
+  public class DadosEmp
+  {
+    public static string Nomee()
+    {
+
+      string name;
+
+      Console.WriteLine ( "Escreva seu nome : ");
+      name = Console.ReadLine();
+      return name;
+    }
+
+    public static int Salario ()
+    {
+      int sal = 0;
+      Console.WriteLine ("Qual seu salario");
+      sal = Convert.ToInt32(Console.ReadLine());
+
+      return sal;
+
+    }
+
+  }
+
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ExercicioB
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      string nameA, nameB;
+      int salA = 0, salB = 0;
+      int resultado = 0;
+
+      nameA = DadosEmp.Nomee();
+      salA = DadosEmp.Salario();
+
+      nameB = DadosEmp.Nomee();
+      salB = DadosEmp.Salario();
+
+      resultado = (salA + salB) /2;
+
+
+
+      Console.WriteLine(resultado.ToString("C"));
+    }
+  }
+}
